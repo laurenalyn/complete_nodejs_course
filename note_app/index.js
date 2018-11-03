3be04288b1b9291dd7e0cec7cf31fead0c39eafe
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-03 00:17:02 -0400

    Adds our age to the end of the string from the notes.js file

--- note_app/index.js
+++ note_app/index.js
@@ -1,13 +1,10 @@
-console.log('starting app');
+console.log('Starting index.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
-// console.log(user);
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
-// you can also write fs.appendFile('greetings.txt', 'Hello world!', function(err) {
-//     if (err){
-//         console.log('error');
-//     }
-// });
+let user = os.userInfo();
+
+fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+
