7d67e85aa34f11dc3d35e3f7d192581531ee95f5
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-04 12:32:20 -0500

    adds error handling and functionality for addNote

--- note_app/notes.js
+++ note_app/notes.js
@@ -1,7 +1,33 @@
+// starts notes.js
 console.log(`Starting notes.js`);
+// imports node module
+const fs = require('fs');
 
+// function expression with an anonymous function that accepts two arguments title and body
 let addNote = (title, body) => {
-    console.log('Adding Note', title, body);
+    // saves an empty array and an object inside our function
+    let notes = [];
+    let note = {
+        title,
+        body
+    };
+    
+    // We add the try catch for error handeling. If the file does not exsist yet then it will move on to the line where we push the note
+    // into our notes object. The next step would be to run fs.writeFileSync which creates the notes-data.json file and we pass the notes
+    // object to the file and it gets converted to json prior to being written to the file. 
+    // If the file already exsists then we go into our try/catch statement and read our current file notes-data.json
+    // The current json string insdie that file gets assigned to noteString. From there we parse it back into a javascript object
+    // And then we push that object into our notes array and proceed to fs.writeFileSync which will add our whole array with the new note 
+    // added and converted back into json
+    try {
+        let notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 };
 
 let getAll = () => {
