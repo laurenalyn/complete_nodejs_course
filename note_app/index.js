1f19af5c7f7ab2b6bde600798ec1b38d49d9becb
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-03 21:25:12 -0400

    adds new commands for us to be able to call in the terminal

--- note_app/index.js
+++ note_app/index.js
@@ -3,12 +3,30 @@ console.log('Starting index.js');
 const fs = require('fs');
 const os = require('os');
 const _ = require('lodash');  //3rd party module
-const notes = require('./notes.js');
+const notes = require('./notes.js'); //local module
 
-console.log(_.isString(true)); //false
-console.log(_.isString('Lauren'));  //true
+// console.log(_.isString(true)); //false
+// console.log(_.isString('Lauren'));  //true
 
-let filteredArray = _.uniq(['lauren', 1, 'lauren']);
-console.log(filteredArray);  //['lauren', 1]
+// _.uniq is method of the _ that eliminates duplicates within an array 
+// let filteredArray = _.uniq(['Mike']);
+// console.log(filteredArray); 
 
 
+// Start with argv[2] & higher - argv[0] & argv[1] cannot be reassigned for new commands
+// process.argv is part of the Node environment, not a 3rd party module
+let command = process.argv[2];
+console.log('Command: ', command);
+// console.log(process.argv);
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Note being read');
+} else if (command === 'remove') {
+    console.log('Removing note');
+} else {
+    console.log('Command not recgonized')
+}
