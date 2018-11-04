11f7477430fb69a5f3b685723d5494ebd3274769
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-04 16:31:27 -0500

    refactored code to be able to reuse code

--- note_app/notes.js
+++ note_app/notes.js
@@ -3,15 +3,7 @@ console.log(`Starting notes.js`);
 // imports node module
 const fs = require('fs');
 
-// function expression with an anonymous function that accepts two arguments title and body
-let addNote = (title, body) => {
-    // saves an empty array and an object inside our function
-    let notes = [];
-    let note = {
-        title,
-        body
-    };
-
+const fetchNotes = () => {
     // We add the try catch for error handeling. If the file does not exsist yet then it will move on to the line where we push the note
     // into our notes object. The next step would be to run fs.writeFileSync which creates the notes-data.json file and we pass the notes
     // object to the file and it gets converted to json prior to being written to the file. 
@@ -21,24 +13,39 @@ let addNote = (title, body) => {
     // added and converted back into json
     try {
         let notesString = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(notesString);
+       return notes = JSON.parse(notesString);
     } catch (e) {
-
+        return [];
     }
+};
 
-    // will store an array with all notes that already exist inside of the notes array that have the title of the note that is trying to be created
+const saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+// function expression with an anonymous function that accepts two arguments title and body
+let addNote = (title, body) => {
+    // saves an empty array and an object inside our function
+    let notes = fetchNotes();
+    let note = {
+        title,
+        body,
+    };
+
+    // below variable (duplicateNotes) will store an array with all notes that already exist inside of the notes array that have the title of the note that is trying to be created
     // let duplicateNotes = notes.filter((note) => {
     //     return note.title === title;
     // });
 
     // short hand for (above) arrow syntax body since we are only returning one line of code to execure
     let duplicateNotes = notes.filter((note) => note.title === title);
-    
+
     // if the length of our duplicateNotes === 0 meaning we have not yet duplicated the title name then we want to go ahead and execute our if statements - 
     // otherwise if we have duplicated the title name then the data will not be pushed
     if (duplicateNotes.length === 0) {
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+        saveNotes(notes);
+        return note;
     } 
 };
 
