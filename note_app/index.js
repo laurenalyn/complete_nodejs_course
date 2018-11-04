dec1a4ba98fc9edfcba3035fe7b9ebc52abfc3fc
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-03 21:46:02 -0400

    uses the yargs npm to parse values

--- note_app/index.js
+++ note_app/index.js
@@ -1,10 +1,12 @@
 console.log('Starting index.js');
-
+//built in modules
 const fs = require('fs');
 const os = require('os');
-const _ = require('lodash');  //3rd party module
-const notes = require('./notes.js'); //local module
-
+//3rd party module
+const _ = require('lodash');
+const yargs = require('yargs');
+ //local module
+const notes = require('./notes.js');
 // console.log(_.isString(true)); //false
 // console.log(_.isString('Lauren'));  //true
 
@@ -12,12 +14,15 @@ const notes = require('./notes.js'); //local module
 // let filteredArray = _.uniq(['Mike']);
 // console.log(filteredArray); 
 
+// Uses yargs npm to parse passed values
+const argv = yargs.argv;
 
 // Start with argv[2] & higher - argv[0] & argv[1] cannot be reassigned for new commands
 // process.argv is part of the Node environment, not a 3rd party module
 let command = process.argv[2];
 console.log('Command: ', command);
-// console.log(process.argv);
+console.log('process',process.argv);
+console.log('yargs', argv);
 
 // Checks if the command passed is equal to the following: 
 if (command === 'add') {
