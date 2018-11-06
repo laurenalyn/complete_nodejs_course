f80379a55b21bde9e3ad5543d8643bdd642d9cad
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-05 22:11:36 -0500

    changes root in request to home page also created an about pg

--- node_web_server/server.js
+++ node_web_server/server.js
@@ -1,25 +1,28 @@
 const express = require('express');
+const hbs = require('hbs');
 
 let app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express</h1>');
-    res.send({
-        name: 'Lauren',
-        likes: [
-            'coffee',
-            'food'
-        ]
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the home page',
+        currentYear: new Date().getFullYear()
     });
 });
 
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear(),
+    });
 });
 
 app.get('/bad', (req,res) => {
