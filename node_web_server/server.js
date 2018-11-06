f285cc7db58a350d3de2be57dc498b4eafe657d1
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-05 21:36:41 -0500

    adds help pg for viewer and logs when server is connected

--- node_web_server/server.js
+++ node_web_server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 let app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+
+
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express</h1>');
     res.send({
@@ -23,4 +27,6 @@ app.get('/bad', (req,res) => {
         errorMessage: 'Unable to handle request.'
     });
 });
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
