6b8805c5e866e4827944ed71f10635442b8324b4
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-05 23:09:15 -0500

    tied in partials, and created a helper function

--- node_web_server/server.js
+++ node_web_server/server.js
@@ -3,25 +3,31 @@ const hbs = require('hbs');
 
 let app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
 
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express</h1>');
     res.render('home.hbs',{
         pageTitle: 'Home Page',
-        welcomeMessage: 'Welcome to the home page',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: 'Welcome to the home page'
     });
 });
 
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear(),
+        pageTitle: 'About Page'
     });
 });
 
