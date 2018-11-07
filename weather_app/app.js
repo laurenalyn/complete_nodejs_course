466611117076f271b7400a03171cbd98ba621c3c
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-06 20:55:24 -0500

    API call to mapquest

---
+++ weather_app/app.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request({
+    url: 'http://www.mapquestapi.com/geocoding/v1/address?key=yBhtlHjM1bPrsZdpmAvGlqoG7mrW4Vyw&location=1301%20lombard%20street%20philadelphia&fbclid=IwAR3KoGjr_3fe4JB0XJnj9BdVztH5Envd05EQVxvwQcQLtdkYcjo0t2g55Yo',
+    json: true
+}, (error, response, body) => {
+    console.log(body);
+});
