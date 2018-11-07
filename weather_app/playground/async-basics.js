7bf44868cb594d756ebf166fb37b71f674f9ed9f
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-06 19:02:31 -0500

    set up playground folder

---
+++ weather_app/playground/async-basics.js
@@ -0,0 +1,13 @@
+console.log('starting app');  //we see this first
+
+setTimeout( () => {
+    console.log('inside of callback'); // we see this fourth
+}, 2000);
+
+setTimeout( () => {
+    console.log('second callback'); // we see this second - it was stil in "timeout"
+}, 0); 
+
+console.log('finishing app'); // we see this second
+
+
