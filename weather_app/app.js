81959976c2a098ea23137b433aa1c06f8c271026
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-06 20:59:08 -0500

    JSON.stringified our body from api call

--- weather_app/app.js
+++ weather_app/app.js
@@ -4,5 +4,5 @@ request({
     url: 'http://www.mapquestapi.com/geocoding/v1/address?key=yBhtlHjM1bPrsZdpmAvGlqoG7mrW4Vyw&location=1301%20lombard%20street%20philadelphia&fbclid=IwAR3KoGjr_3fe4JB0XJnj9BdVztH5Envd05EQVxvwQcQLtdkYcjo0t2g55Yo',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 });
