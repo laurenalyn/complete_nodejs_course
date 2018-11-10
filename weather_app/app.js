c73e067b9df5e8dd746de6d648bd018759564792
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-09 19:36:20 -0500

    error handling

--- weather_app/app.js
+++ weather_app/app.js
@@ -20,8 +20,16 @@ request({
     url: `http://www.mapquestapi.com/geocoding/v1/address?key=yBhtlHjM1bPrsZdpmAvGlqoG7mrW4Vyw&location=${encodeAddress}&fbclid=IwAR3KoGjr_3fe4JB0XJnj9BdVztH5Envd05EQVxvwQcQLtdkYcjo0t2g55Yo`,
     json: true
 }, (error, response, body) => {
+    if (error) {
+        // will run if error exists
+        console.log('Unable to connect to Google servers.');
+    } else if (body.results[0].locations[0].geocodeQualityCode !== 'P1AAA') {
+        console.log('Unable to find that address');
+    } else if (body.results[0].locations[0].geocodeQualityCode === 'P1AAA'){
+
     // console.log(JSON.stringify(body, undefined, 2));
     console.log(`Address: ${JSON.stringify(body.results[0].providedLocation.location)}`);
     console.log(`Latitude: ${JSON.stringify(body.results[0].locations[0].latLng.lat)}`);
     console.log(`Longitude: ${JSON.stringify(body.results[0].locations[0].latLng.lng)}`);
+    }
 });
